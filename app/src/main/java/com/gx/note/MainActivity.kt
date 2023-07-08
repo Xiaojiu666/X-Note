3a0fc6a84d20469f589cbd297997f1c230fa26df
xu <xu.guo@1step.ai>
2023-07-08 16:24:06 +0800

    feat: [study] ~

--- app/src/main/java/com/gx/note/MainActivity.kt
+++ app/src/main/java/com/gx/note/MainActivity.kt
@@ -7,9 +7,19 @@ import android.provider.AlarmClock
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.AndroidUiDispatcher
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -17,6 +27,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.gx.note.home.Foo
+import com.gx.note.home.HomePage
+import com.gx.note.home.HomePageRoute
 import com.gx.note.ui.LocalGlobalNavController
 import com.gx.note.ui.RouteConfig
 import com.gx.note.ui.utils.KeyboardHandler
@@ -29,11 +42,15 @@ import kotlinx.coroutines.runBlocking
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            HomeNav()
+            CustomView()
+
+//            HomePageRoute()
+//            HomeNav()
 //            XNoteTheme {
 //                val viewModel: DiaryHomeViewModel by viewModels()
 //                DiaryHomePage(viewModel)
